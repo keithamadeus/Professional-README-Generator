b2f3fb970d76ebd24eb43f360ed2ec84f9c764c7
Keith <keith_a_w@hotmail.com>
2024-08-20 09:40:38 -0600

    add array of questions

--- index.js
+++ index.js
@@ -1,7 +1,78 @@
 // TODO: Include packages needed for this application
+import inquirer from 'inquirer';
+import fs from 'fs';
+import generateMarkdown from './utils/generateMarkdown.js';
+import { type } from 'os';
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please provide a description of your project.',
+    },
+    {
+        type: 'input',
+        name: 'tableOfContents',
+        message: 'Please provide a table of contents for your project.',
+    },
+    {
+        type: 'input',
+        name: 'codeRefactorExample',
+        message: 'Please provide an example of code refactoring.',
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Please provide installation instructions for your project.',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Please provide usage information for your project.',
+    },
+    {
+        type: 'input',
+        name: 'learningPoints',
+        message: 'Please provide learning points for your project.',
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'Please provide contribution guidelines for your project.',
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Please provide test instructions for your project.',
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Please choose a license for your project.',
+        choices: ['MIT', 'GNU GPLv3', 'Apache 2.0', 'ISC', 'None'],
+    },
+    {
+        type: 'input',
+        name: 'authorInfo',
+        message: 'Please provide author information for your project.',
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'Please enter your GitHub username.',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Please enter your email address.',
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
