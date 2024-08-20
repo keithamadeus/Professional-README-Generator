79b041e754c8d70106265b1191523d8811157da8
Keith <keith_a_w@hotmail.com>
2024-08-20 11:04:52 -0600

    add ReadMe and init functions with licence badge function

--- index.js
+++ index.js
@@ -75,10 +75,22 @@ const questions = [
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) =>
+        err ? console.error(err): console.log('ReadMe file created!'))
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+        .then((answers) => {
+            const markdown = generateMarkdown(answers);
+            writeToFile('README.md', markdown);
+        })
+        .catch((error) => {
+            console.error(error);
+        });
+}
 
 // Function call to initialize app
 init();
